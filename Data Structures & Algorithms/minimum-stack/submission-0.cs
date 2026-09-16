abf4ac2bec03cd3ea1f5a12206eeb36875b79c63
Farhan Ali <acfarhan123@gmail.com>
2026-09-16 23:25:56 +0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cs
@@ -0,0 +1,32 @@
+public class MinStack {
+    private Stack<int> stack;
+    private Stack<int> minStack;
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack=new Stack<int>();
+    }
+
+    public void Push(int val) {
+        stack.Push(val);
+        if(minStack.Count==0 || val<= minStack.Peek())
+        {
+            minStack.Push(val);
+        }
+        else
+        minStack.Push(minStack.Peek());
+
+    }
+
+    public void Pop() {
+        stack.Pop();
+        minStack.Pop();
+    }
+
+    public int Top() {
+        return stack.Peek();
+    }
+
+    public int GetMin() {
+        return minStack.Peek();
+    }
+}
