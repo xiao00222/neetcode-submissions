6558157581402f8645a09913bd2510275c93c06f
Farhan Ali <acfarhan123@gmail.com>
2026-09-23 19:17:16 +0500

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.Length && j < t.Length) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else
+                j++;
+        }
+        return i == s.Length;
+    }
+}
